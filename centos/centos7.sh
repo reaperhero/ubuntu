3806aae778432517e08f1233cf34ed34d4acc0fe
chenqiangjun <chenqiangjun@mudu.tv>
2020-05-15 22:40:37 +0800

    chenqiangjun update

---
+++ centos/centos7.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rm -fr /etc/yum.repos.d/*
+wget -O /etc/yum.repos.d/CentOS-Base.repo https://mirrors.aliyun.com/repo/Centos-7.repo
+yum update -y
---
+++ ntp/ntp.sh
@@ -0,0 +1,2 @@
+#!/bn/bash
+0 1 * * * /usr/sbin/ntpdate ntp1.aliyun.com
