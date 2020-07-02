eb0bd6319daeecac35a91a8545f6cd14b2c77b3b
reaperhero <43801667+reaperhero@users.noreply.github.com>
2020-07-02 21:03:52 +0800

    Update ecs-monitor.sh

--- monitor/ecs-monitor.sh
+++ monitor/ecs-monitor.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
+export PATH=${PATH}
 
-cpuusage=`top -n 1|awk -F '[ %]+' 'NR==3 {print $3}'`
 
-echo $cpuusage
+# 获取cpu idle 
+# 88.94 90.00 89.90 85.58 89.80
+logicStatus=`sar -P ALL 1 1|awk 'NR>2{ if($NF>0||$NF==id) print $NF}'|awk -v RS= '{gsub(/\n/, " "); {print $0}}'|head -1`
+echo ${logicStatus}
+
+# 获取内存使用率
+# 43
+free_Use=`free -m|awk 'NR==2{ print int($3 / $2 * 100)}'`
+echo ${free_Use}
+
+# 获取磁盘最高使用率
+
+disk_Use=`df -h |sort -nrk 5 -nrk 2|awk -F "[ %]+" 'NR==1{print $(NF-1)}'`
+echo ${disk_Use}
