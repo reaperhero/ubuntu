336f6dd5c3fd714de04e1f4ea7d1c2fafa1ee2b8
chenqiangjun <chenqiangjun@mudu.tv>
2020-12-10 10:16:44 +0800

    chenqiangjun update

--- command/git.sh
+++ command/git.sh
@@ -3,3 +3,10 @@
 
 
 git checkout v1.17.14 # 切换到指定tag
+
+
+
+
+# use http
+git config --global url."https://guest:password@github.com/".insteadOf "https://github.com/" 
+git remote set-url origin https://github.com/reaperhero/test.git
