4bb95b14cf6cb95888aa8f66277fa74bc661d28e
chenqiangjun <chenqiangjun@mudu.tv>
2021-04-16 21:28:16 +0800

    chenqiangjun update

--- command/sed.sh
+++ command/sed.sh
@@ -5,16 +5,24 @@
 sed -i '/git.163.com/d' /etc/hosts          # 删除指定内容行
 sed -i "24s/^/#/g"      application.conf    # 注释第24行
 
-sed -i 's/.//'  file                        # 删除每行第一个字符。
+sed -i 's/.//'  file                        # 删除每行第一个字符
+sed 's/.\{5\}//' tmp.txt                    # 删除每行前k个字符，例如k=5
 
 sed -i "45,77s/^/#/g" nginx-configmap.yaml  # 45行到77行，行首添加注释符#   
-
 
 find . -name "*.htm" | xargs sed -i -e "s%somebody%donal%g"   # 递归批量替换
 
 sed -i 'Nd' filename   # 删除第N行
 
-sed -i 'M,Nd' filename # 删除第M到N行：
+sed -i '$d' file.txt   # 删除最后一行
+
+sed -i 'M,Nd' filename # 删除第M到N行
+
+sed -i '2 ittt'  a.txt # 在第2行前插入ttt
+
+sed -i '2 attt'  a.txt # 在第2行后插入ttt
+
+sed -i '2 a\ ttt'  a.txt # 在第2行后插入 ttt
 
 sed -i '3s/aaa/fff/' file      # 表示针对file文件中的第三行，将其中的aaa替换为fff
 
