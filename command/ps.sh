cbd280b32a97a6f0e4fa0080864e902828e61811
manlu <manlu@dtstack.com>
2022-04-02 16:39:41 +0800

    chenqiangjun update

--- command/ps.sh
+++ command/ps.sh
@@ -5,3 +5,6 @@ ps aux --sort=-%cpu       # 按CPU降序排列
 
 ps -ef --sort=-start_time # 按启动时间降序
 ps -ef -ww                # 显示完整命令
+
+
+ps -eo pid,lstart,etime,cmd | grep nginx   # 显示进程的启动时间
--- mysql/common.sql
+++ mysql/common.sql
@@ -19,20 +19,44 @@ select * from 表A inner join 表B on 判断条件;
 
 -- 左外连接（left join）以左表为主表（查询全部）， 右表为辅表（没有的显示null）
 
-SQL：select * from 表A left join 表B on 判断条件;
+select * from 表A left join 表B on 判断条件;
 
 -- 右外连接（right join）
 
 -- 以右表为主表（查询全部）， 左表为辅表（没有的显示null）
 
-SQL：select * from 表A right join 表B on 判断条件;
+select * from 表A right join 表B on 判断条件;
 
 -- 全连接（full join）
 
 -- 两个表的所有数据都展示出来
 
-SQL：select * from 表A full join 表B on 判断条件;
+select * from 表A full join 表B on 判断条件;
 
 -- 联合(union / union all）
 
-union 操作符合并的结果集，不会允许重复值，如果允许有重复值的话，使用UNION ALL
+-- union 操作符合并的结果集，不会允许重复值，如果允许有重复值的话，使用UNION ALL
+
+select * from A   
+union
+select * from B
+
+
+
+
+-- 前缀索引
+
+ALTER TABLE table_name ADD KEY(column_name(prefix_length));
+
+
+
+
+-- exists用于对外表记录做筛选。exists会遍历外表，将外查询表的每一行，代入内查询进行判断
+-- exist类似于where条件，但是可以数据可以不是当前表
+-- 当exists里的条件语句能够返回记录行时，条件就为真，返回外表当前记录。
+-- 反之如果exists里的条件语句不能返回记录行，条件为假，则外表当前记录被丢弃。
+select a.* from A a where exists(select 1 from B b where a.id=b.id)
+
+
+-- in是先把后边的语句查出来放到临时表中，然后遍历临时表，将临时表的每一行，代入外查询去查找。
+select * from Awhere id in(select id from B)
