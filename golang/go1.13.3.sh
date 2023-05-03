bb9c0caf1ce1b1a235349788f51b355e66d99964
manlu <manlu@dtstack.com>
2023-05-03 11:29:38 +0800

    chenqiangjun update

--- golang/go1.13.3.sh
+++ golang/go1.13.3.sh
@@ -21,3 +21,14 @@ echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >>/root/.bashrc
 source /root/.bashrc
 mkdir /root/go
 go version
+
+
+wget https://golang.google.cn/dl/go1.17.7.linux-arm64.tar.gz
+tar -xvf go1.17.7.linux-arm64.tar.gz
+mv go /usr/local
+echo 'export GOROOT=/usr/local/go'>>/root/.bashrc
+echo 'export GOPATH=/root/go' >>/root/.bashrc
+echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >>/root/.bashrc
+source /root/.bashrc
+mkdir /root/go
+go version
--- shell/help.sh
+++ shell/help.sh
@@ -6,7 +6,9 @@
 #Print the result
 echo $sum
 
-
+for((i=0;i<3;i++));do
+  touch test_$i.txt
+done
 
 if [ -e /tmp ]
 then
@@ -40,3 +42,11 @@ for i in `seq 10`; do curl -v http://baidu.com; done
 echo ${my_name:0:2}  # 截取字符串
 echo ${#my_name}     # 获取字符串长度
 
+
+if [[ "5" > "4" ]] && [[ "1" == "1" ]] && [ -f "user.md" ]
+then
+    echo 1
+else
+    echo 2
+
+fi
